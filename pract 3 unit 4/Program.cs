329392d0b4090e6d2026a48539511c5229453f88
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-03-04 21:52:37 -0600

    Agregar archivos de proyecto.

---
+++ pract 3 unit 4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circulo miCirculo = new Circulo();
+        miCirculo.Radio = 5;
+        double area = miCirculo.CalcularArea(); // Método implementado de la clase abstracta Figura
+
+        Console.WriteLine($"El área del círculo es {area}.");
+        Console.ReadKey();
+    }
+}
---
+++ pract 3 unit 4/figura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+// Clase abstracta
+public abstract class Figura
+{
+    // Método abstracto
+    public abstract double CalcularArea();
+}
+
+// Clase que hereda de Figura e implementa el método abstracto
+public class Circulo : Figura
+{
+    public double Radio { get; set; }
+
+    public override double CalcularArea()
+    {
+        return Math.PI * Math.Pow(Radio, 2);
+    }
+}
+
